650ab14230ee462d366bdd9db76f94102341e19a
Honza Volmut <jan.volmut@gmail.com>
2014-02-09 19:58:02 +0100

    Errors reporting
    - If config.ini is missing, error message is shown
    - Twitter api request surrounded by try/catch block, error is shown when Exception is thrown

--- src/main.php
+++ src/main.php
@@ -2,7 +2,17 @@
 
 require_once "../vendor/autoload.php";
 
-$settings = parse_ini_file('../config.ini');
+const HTML_NEW_LINE = "<br />";
+const CONFIG_FILE = '../config.ini';
+
+$settings = parse_ini_file(CONFIG_FILE);
+
+if (!is_array($settings)) {
+	echo "Do you have your '" . CONFIG_FILE . "' created properly?";
+	echo HTML_NEW_LINE;
+	echo "Humbly dying...";
+	exit;
+}
 
 // Settings has to be an associative array, config may look like this:
 // oauth_access_token = ...
@@ -16,15 +26,20 @@ $requestMethod = 'GET';
 // I am afraid the alphabetical order might matter, because of oAuth
 $getField = '?screen_name=VolmutJ';
 
-$api = new TwitterAPIExchange($settings);
+try {
+	$api = new TwitterAPIExchange($settings);
 
-// Fields must be set first, in order for oAuth to be able to calculate the signature
-$jsonResult = $api
-    ->setGetfield($getField)
-    ->buildOauth($url, $requestMethod)
-    ->performRequest();
+	// Fields must be set first, in order for oAuth to be able to calculate the signature
+	$jsonResult = $api
+	    ->setGetfield($getField)
+	    ->buildOauth($url, $requestMethod)
+	    ->performRequest();
+
+} catch(Exception $e) {
+	echo $e->getMessage();
+	exit;
+}
 
 // Enjoy the happiness
 $result = json_decode($jsonResult);
 echo $jsonResult;
-//var_dump($result);
