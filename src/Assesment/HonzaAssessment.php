4c1220ba84b1e4bac02adb23e3a9f271abc8fd70
Pavel Smolka <pavel.smolka@gmail.com>
2014-02-22 19:16:24 +0000

    Honza and Pavel Assessment classes

---
+++ src/Assesment/HonzaAssessment.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Flock\Assessment;
+
+use stdClass;
+
+class HonzaAssessment implements IAssessment {
+
+    /**
+     * Decides how to treat a tweet:
+     *  Q: Where to publish?
+     *  A: List of IDs where to publish.
+     *
+     * @param stdClass $tweet A tweet
+     * @return int[] Array of account IDs where the tweet should be published
+     */
+    public function assess(stdClass $tweet) {
+        // TODO: Implement assess() method.
+    }
+}
